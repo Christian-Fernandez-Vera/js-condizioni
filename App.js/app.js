f5316b14e92ad6a82d8df5f524218d3a137d6b8c
Christian Fernandez Vera <xtian.fdz@hotmail.com>
2026-08-01 00:14:33 +0200

    feat: add basic age validation script and boilerplate HTML structure

---
+++ App.js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Esercizio 1: Chiedi l'età dell'utente tramite prompt. Se l'utente è maggiorenne, stampa a console "Accesso consentito al locale".
+ * Esercizio 2:
+ * 
+ * 
+ * 
+ * 
+ */
+
+// console.log('JS ok!')
+
+const etaUtente = Number(prompt("Esercizio 1: inserisci la tua età"));
+if (etaUtente >= 18) {
+    console.log('Accesso consentito al locale');
+}
+
+
