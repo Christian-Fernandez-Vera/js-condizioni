c6bda7b228ae3cb7b6a863f02f2e4ba192282af5
Christian Fernandez Vera <xtian.fdz@hotmail.com>
2026-08-01 00:58:49 +0200

    feat: add baggage fee logic for exercise 2 and comment out previous exercise code

--- App.js/app.js
+++ App.js/app.js
@@ -1,17 +1,28 @@
 /**
  * Esercizio 1: Chiedi l'età dell'utente tramite prompt. Se l'utente è maggiorenne, stampa a console "Accesso consentito al locale".
  * Esercizio 2:
- * 
+ * Il costo del biglietto è di 36 €. Chiedi il peso della valigia in kg. Se il peso supera i 23kg, c'è un supplemento di 20 €, infine stampa il prezzo del biglietto.
  * 
  * 
  * 
  */
 
 // console.log('JS ok!')
+// ==========================================
 
-const etaUtente = Number(prompt("Esercizio 1: inserisci la tua età"));
-if (etaUtente >= 18) {
-    console.log('Accesso consentito al locale');
+// const etaUtente = Number(prompt("Esercizio 1: inserisci la tua età"));
+// if (etaUtente >= 18) {
+//     console.log('Accesso consentito al locale');
+// }
+
+
+// ==========================================
+let costoBiglietto = 36;
+const pesoValigia = Number(prompt("Esercizio 2: inserisci il peso della valigia in kg"));
+const supplemento = 20
+if (pesoValigia > 23) {
+    console.log('Supplemento di 20 €');
 }
+
 
 
